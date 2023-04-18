6706bf792c9c2c42b84360fa660e4f76d3c752e2
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-04-18 14:19:12 +0100

    add interactive map, allow users to click on continents to call a function

---
+++ static/js/script.js
@@ -0,0 +1,29 @@
+function clicked(region) {
+
+    switch (region) {
+        case "asia":
+            alert("this is asia");
+            break;
+
+        case "europe":
+            alert("this is europe");
+            break;
+
+        case "africa":
+            alert("this is africa");
+            break;
+
+        case "north_america":
+            alert("this is north_america");
+            break;
+
+        case "south_america":
+            alert("this is south_america");
+            break;
+
+        case "australia":
+            alert("this is australia");
+            break;
+
+    }
+}
