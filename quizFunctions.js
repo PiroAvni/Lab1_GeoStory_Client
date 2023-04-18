a7d6823aa2d84229fd2432fbed39217553072ad0
“danteoguns11” <danteoguns@gmail.com>
2023-04-18 18:19:45 +0100

    add functions for popups

---
+++ quizFunctions.js
@@ -0,0 +1,18 @@
+let hint = document.getElementById('hintPopup');
+let fact = document.getElementById('factPopup');
+
+function openHint() {
+    hint.classList.add("open-popup");
+}
+
+function closeHint() {
+    hint.classList.remove("open-popup");
+}
+
+function openFact() {
+    fact.classList.add("open-popup");
+}
+
+function closeFact() {
+    fact.classList.remove("open-popup");
+}
