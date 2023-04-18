cf6c52742acab43dd337a869bc5ec202fcac3b4c
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-04-18 17:27:55 +0100

    fetch random image from API based on continent and append to DOM

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,24 @@
-function clicked(region) {
-    const url = "../pages/continents/" + region + ".html";
+const container = document.querySelector('.container');
+
+function clicked(continent) {
+    const url = `../pages/continents/${continent}.html`;
     window.location.href = url;
+}
+
+function randImg(continent) {
+    fetch(`http://localhost:3000/${continent}/random`)
+        .then(response => response.json())
+        .then(grabImg)
+        .catch(error => {
+            console.error('Error fetching image:', error);
+        });
+}
+
+function grabImg(data) {
+    const randIdx = Math.floor(Math.random() * data.img.length);
+    const imgRand = data.img[randIdx];
+    console.log(imgRand);
+    const img = document.createElement("img");
+    img.src = imgRand["num"];
+    container.appendChild(img);
 }
