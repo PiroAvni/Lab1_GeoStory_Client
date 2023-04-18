1124f1b76a1afa8f2500bfa4d7911446cd849e51
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-04-18 15:22:43 +0100

    added pages for continents, cleaned up directory, refactored script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,4 @@
 function clicked(region) {
-
-    switch (region) {
-        case "asia":
-            alert("this is asia");
-            break;
-
-        case "europe":
-            alert("this is europe");
-            break;
-
-        case "africa":
-            alert("this is africa");
-            break;
-
-        case "north_america":
-            alert("this is north_america");
-            break;
-
-        case "south_america":
-            alert("this is south_america");
-            break;
-
-        case "australia":
-            alert("this is australia");
-            break;
-
-    }
+    const url = "../pages/continents/" + region + ".html";
+    window.location.href = url;
 }
