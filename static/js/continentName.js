a68e29aefba9cde1471122bb0cc1cf9a627c9f19
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-04-20 14:28:29 +0100

    fix hint, fun fact and multiple choice, combined scripts into grabData.js

--- static/js/continentName.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    const urlSegments = window.location.pathname.split('/');
-    const lastSegment = urlSegments[urlSegments.length - 1];
-    const continent = lastSegment.split('.')[0];
-    randImg(continent);
-    fetchFact(continent);
-    fetchHint(continent);
-    fetchPlace(continent);
-}
